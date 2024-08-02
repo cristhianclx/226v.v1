0e853471a06cc7c0e928b7950239de775ddd77ba
Cristhian <cristhianclx@gmail.com>
2024-08-01 21:46:26 -0500

    03

--- 03/src/web/views.py
+++ 03/src/web/views.py
@@ -9,9 +9,9 @@ import pandas as pd
 import pickle
 
 
-job_matrix = pickle.load(open('./src/web/data/job_matrix.pickle', 'rb'))
-job_vectorizer = pickle.load(open('./src/web/data/job_vectorizer.pickle', 'rb'))
-ranker = pd.read_pickle('./src/web/data/puestos.pickle')
+job_matrix = pickle.load(open("./src/web/data/job_matrix.pickle", "rb"))
+job_vectorizer = pickle.load(open("./src/web/data/job_vectorizer.pickle", "rb"))
+ranker = pd.read_pickle("./src/web/data/puestos.pickle")
 
 
 class HomeView(View):
@@ -30,22 +30,22 @@ class HomeView(View):
             ranking = cosine_similarity(cv_matrix, job_matrix, True)
             ranking_serie = pd.Series(ranking[0])
             ranker["RANKING"] = ranking_serie
-            ranker_final = ranker.sort_values('RANKING', ascending=False)
+            ranker_final = ranker.sort_values("RANKING", ascending=False)
             for r in ranker_final.index:
                 if ranker_final["RANKING"][r] >= 0.3:
                     Match.objects.create(
-                        search = instance,
-                        position = ranker_final["PUESTO"][r],
-                        url = ranker_final["URL"][r],
-                        ranking = ranker_final["RANKING"][r] * 100,
+                        search=instance,
+                        position=ranker_final["PUESTO"][r],
+                        url=ranker_final["URL"][r],
+                        ranking=ranker_final["RANKING"][r] * 100,
                     )
-            return redirect('results', id=instance.id)
+            return redirect("results", id=instance.id)
         else:
             return redirect("home")
 
 
 class ResultsView(View):
     def get(self, request, id):
-        instance = Search.objects.get(id = id)
-        matches = Match.objects.filter(search = instance).all().order_by("-ranking")
+        instance = Search.objects.get(id=id)
+        matches = Match.objects.filter(search=instance).all().order_by("-ranking")
         return render(request, "results.html", {"matches": matches})
